b332651d0c6c6137f7235b50bb2184739dcf7f59
vinz86 <vincenzo.disalvatore@gmail.com>
2024-01-28 13:14:21 +0100

    Aggiunti commenti

--- VdsServerFiles.php
+++ VdsServerFiles.php
@@ -1,5 +1,13 @@
 <?php
 
+// Esempio
+//$ServerFiles = new VdsServerFiles();
+//$ServerFiles->download_file("https://codeload.github.com/vinz86/VdsServerFiles-PHP-Class/zip/refs/heads/main", "file.zip", true, true);
+
+/**
+ * Classe VdsServerFiles: manipolazione files PHP
+ *
+ */
 class VdsServerFiles {
 	private $url;
 	private $filename;
@@ -10,7 +18,15 @@ class VdsServerFiles {
 	public function __construct(){
 	}
 
-
+	/**
+	 * Scarica un file dal server, se il file è uno zip può estrarlo.
+	 *
+	 * @param {string} url Url del file.
+	 * @param {string} filename Il nome con il quale verrà salvato il file.
+	 * @param {boolean} unzip Estrai o meno il file zip.
+	 * @param {boolean} deleteZipIfUnzip filename Cancella o meno il file zip dopo l'estrazione.
+	 * @return {string} Stringa con stato dell'operazione.
+	 */
 	public function download_file($url, $filename, $unzip=false, $deleteZipIfUnzip=false){
 		if ((!$url) || (!$filename)) exit();
 
@@ -36,6 +52,13 @@ class VdsServerFiles {
 		}
 	}
 
+	/**
+	 * Estrae file zip.
+	 *
+	 * @param {string} filename Il nome del file da estrarre.
+	 * @param {boolean} deleteZipIfUnzip filename Cancella o meno il file zip dopo l'estrazione.
+	 * @return {string} Stringa con stato dell'operazione.
+	 */
 	public function unzip_file($filename, $deleteZipIfUnzip = false){
 		$unzip = new ZipArchive;
 		$out = $unzip->open($filename);
@@ -61,6 +84,4 @@ class VdsServerFiles {
 
 }
 
-$ServerFiles = new VdsServerFiles();
-$ServerFiles->download_file("https://codeload.github.com/vinz86/Web-Server-ESP8266/zip/refs/heads/main", "file.zip", true, true);
 ?>
